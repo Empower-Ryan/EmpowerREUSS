cef787210d66c07b3177a6abf3ddd73df4502fb7
Ryan Reuss <ryan.reuss@skyline.be>
2023-07-11 10:23:42 -0400

    TEST PUSH

--- EmpowerREUSS_1/EmpowerREUSS_1.cs
+++ EmpowerREUSS_1/EmpowerREUSS_1.cs
@@ -17,7 +17,7 @@ namespace EmpowerREUSS_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("TEST PUSH");
 		}
 	}
 }
